8eb5d34f38fe1f9e3a534c77c6f41e47bbda6aff
murilo henrique <77279094+Murilo4@users.noreply.github.com>
2024-11-13 07:57:14 -0300

    firebase

---
+++ deck_app/firebase_config/firebase.py
@@ -0,0 +1,21 @@
+import firebase_admin
+from firebase_admin import credentials, storage
+import os
+# Inicialize o Firebase Admin SDK
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+})
+firebase_admin.initialize_app(cred, {
+    'storageBucket': os.getenv("STORAGE_BUCKET")})
+
+bucket = storage.bucket()
