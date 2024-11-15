4208ae5c938218fd84c6ea1170ece8a91d034271
murilo henrique <77279094+Murilo4@users.noreply.github.com>
2024-11-15 09:33:21 -0300

    add: reviews

--- deck_app/views/Deck/deck_management.py
+++ deck_app/views/Deck/deck_management.py
@@ -149,10 +149,10 @@ def get_all_decks(request, page_number):
                     **serialized_deck,
                     'situation': user_flashcard.situation if user_flashcard else None,
                     'flashcards': flashcard_count,
-                    'new': user_preferences.new_per_day if user_preferences else 0,
-                    'learning': user_preferences.learning_per_day if user_preferences else 0,
-                    'reviewing': user_preferences.review_per_day if user_preferences else 0,
-                    'favorite': user_deck.favorite if user_deck else 0,
+                    'new': user_deck.new if user_deck else 0,
+                    'learning': user_deck.learning if user_deck else 0,
+                    'reviewing': user_deck.reviewing if user_deck else 0,
+                    'favorite': user_deck.favozrite if user_deck else 0,
                 })
 
             flashcard_min = min(
@@ -212,13 +212,11 @@ def get_min_max_flashcard(request):
             user_decks = UserDeck.objects.filter(user_id=user_id).values_list(
                 'deck_id', flat=True)
             deck_ids = list(user_decks)
-            print(deck_ids)
 
             decks = Deck.objects.filter(id__in=deck_ids)
 
             flashcard_counts = DeckFlashCard.objects.values(
                 'deck_id').annotate(flashcard_count=Count('flashcard_id'))
-            print(flashcard_counts)
 
             # Dicionário para contar flashcards de cada deck
             flashcard_counts_dict = {
@@ -424,11 +422,60 @@ def get_standard_decks(request, page_number):
         return JsonResponse({"success": False,
                              "error": ["Metodo não autorizado"]},
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
-    
+
 
 @csrf_exempt
 @api_view(['GET'])
+def get_reviews(request):
+    if request.method == 'GET':
+        try:
+            token = request.headers.get('Authorization')
+            if not token:
+                return JsonResponse({
+                    'success': False,
+                    'error': ['Token de autorização ausente. Faça login novamente.']
+                }, status=status.HTTP_401_UNAUTHORIZED)
 
+            jwt_data = validate_jwt(token)
+            user_id = jwt_data.get('id')
+            if not user_id:
+                return JsonResponse({'success': False,
+                                     'error': ['userId é necessário']},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
+            user_decks = UserDeck.objects.filter(
+                user_id=user_id).values_list('deck_id', flat=True)
+
+            standard_decks = Deck.objects.filter(
+                type_deck='Standard').exclude(id__in=user_decks)
+
+            reviews_counts_list = []
+            for deck in standard_decks:
+                reviews_counts_list.append(deck.reviews)
+
+            reviews_min = min(
+                reviews_counts_list) if reviews_counts_list else 0
+            reviews_max = max(
+                reviews_counts_list) if reviews_counts_list else 0
+
+            return JsonResponse({"success": True,
+                                 "message": "Valores retornados",
+                                'minReviews': reviews_min,
+                                 'maxReviews': reviews_max},
+                                status=status.HTTP_200_OK)
+
+        except exceptions.NotFound:
+            return JsonResponse({'success': False,
+                                 'error': ['Usuários não encontrados']},
+                                status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return JsonResponse({'success': False,
+                                 'error': f'Erro: {str(e)}'},
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        return JsonResponse({"success": False,
+                             "error": ["Metodo não autorizado"]},
+                            status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 @csrf_exempt
